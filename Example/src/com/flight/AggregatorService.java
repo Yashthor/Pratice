556669ef1e4da4e80f802dad1cd736b02f27ab5f
yash saxena <mymail2yash11@gmail.com>
2023-12-10 17:34:39 +0530

    code fix

--- Example/src/com/flight/AggregatorService.java
+++ Example/src/com/flight/AggregatorService.java
@@ -1,6 +1,7 @@
 package com.flight;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -22,29 +23,19 @@ public class AggregatorService {
 	public List<FlightData> getAllFlightData(String src, String dst) throws InterruptedException, ExecutionException {
 		List<Callable> CallableList = new ArrayList<>();
 		List<FlightData> result = new ArrayList<>();
+		List<Future<FlightData>> listOfFlightData = new ArrayList<>();
 		for (AirlineSearchInterface impl : list) {
-//			CallableList.add(() -> {
-//				return impl.getAllFlightData(src, dst);
-//			});
-			CallableList.add(new Callable() {
-				@Override
-				public List<FlightData> call() throws Exception {
-					return impl.getAllFlightData(src, dst);
-				}
-				
+			CallableList.add(() -> {
+				return impl.getAllFlightData(src, dst);
 			});
-			List<Future<FlightData>> listOfFlightData = new ArrayList<>();
-			for(Callable c : CallableList) {
-				listOfFlightData.add(executorService.submit(c));
-			}
+		}
+		for (Callable c : CallableList) {
+			listOfFlightData.add(executorService.submit(c));
+		}
 
-			for(Future<FlightData> z : listOfFlightData) {
-//				result.add(z.get());
-				System.out.println(z.get());
-				System.out.println(z.getClass().getName());
-			}
+		for (Future<FlightData> z : listOfFlightData) {
+			result.addAll((Collection<? extends FlightData>) z.get());
 		}
 		return result;
-
 	}
 }
